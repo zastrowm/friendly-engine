f7aa4edc24bd08f33a5bfd14e95a4add76a75ec7
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2020-07-22 17:39:14 -0500

    Implement the bare canvas in react

--- v3/src/App.tsx
+++ v3/src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { DesignCanvas } from "./Views/DesignCanvas";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="design-app">
+      <header>
+        <h1>Web App Builder</h1>
+        {/* Render each control as a button that inserts it */}
+        {/*{Array.from(this.descriptors.getDescriptors()).map((d) => (*/}
+        {/*  <button onClick={() => this.addControl(d)}>Add {d.displayName}</button>*/}
+        {/*))}*/}
+        <button>Delete</button>
+        <button>Undo</button>
+        <button>Redo</button>
+        <button>Save Layout</button>
+        <button>Load Layout</button>
+        <button>Reset Canvas</button>
       </header>
+      <main>
+        <div>
+          <DesignCanvas />
+        </div>
+      </main>
+      <aside>
+        <div>Panel</div>
+      </aside>
     </div>
   );
 }
---
+++ v3/src/Views/DesignCanvas.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import "./DesignCanvas.css";
+
+export function DesignCanvas() {
+  return (
+    <div className="design-canvas" />
+  );
+}
