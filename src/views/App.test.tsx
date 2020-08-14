6dcab62b0cc14051ad1d9a91ae6036b68c68fd01
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2020-08-14 12:54:38 -0500

    Remove unused/unmaintained test

--- src/views/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import EditorApp from './EditorApp';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<EditorApp />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
